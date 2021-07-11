f598c3e8a657408287b9de2b47167333ff7d47e7
ალექსანდრე მჭედლიძე <mchedlidze.aleksandr@students.gov.ge>
2021-07-11 13:38:08 +0400

    added search functionality

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 import markdown2
 from markdown2 import Markdown
 from . import util
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
 
 def entry(request, entry):
     markDowner = Markdown()
@@ -20,4 +23,20 @@ def entry(request, entry):
         return render(request, "encyclopedia/entry.html", {
             "entry": markDowner.convert(entryPage),
             "entryTitle": entry
+        })
+
+
+def search(request):
+    value = request.GET.get('q', '')
+    if util.get_entry(value) is not None:
+        return HttpResponseRedirect(reverse("entry", kwargs={"entry": value}))
+    else:
+        subStringQuery = []
+        for entry in util.list_entries():
+            if value.upper() in entry.upper():
+                subStringQuery.append(entry)
+        return render(request, "encyclopedia/index.html", {
+            "entries": subStringQuery,
+            "search": True,
+            "value": value
         })
